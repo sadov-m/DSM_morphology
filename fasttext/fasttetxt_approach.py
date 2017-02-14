618955a50cf1501e9ec6c0b69da3bd503f80199a
akutuzov <akutuzov72@gmail.com>
2017-02-14 01:43:15 +0100

    Fasttext

--- fasttext/fasttetxt_approach.py
+++ fasttext/fasttetxt_approach.py
@@ -1,10 +1,14 @@
+# coding: utf-8
 import fasttext
-import re
+import sys
 
-""""#model = fasttext.cbow('corpus.txt', 'model') #learning
-#print(model.words) # list of words in dictionary
+corpus = sys.argv[1]
 
-model = fasttext.load_model('model.bin')
-print(model.words) #trying to list the words the model knows, though no result
-print(model['что'])
-"""
+
+model = fasttext.cbow(sys.argv[1], 'model') #learning
+print(model.words) # list of words in dictionary
+
+#model = fasttext.load_model('model.bin')
+#print(model.words) #trying to list the words the model knows, though no result
+#print(model['что'])
+#"""
