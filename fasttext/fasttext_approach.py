4bfed58a885f45e37f49c6761fecdc02cb2ce3a8
Mikhail <mikeabyrvalg5@gmail.com>
2017-06-05 16:53:57 +0300

    Update fasttext_approach.py
    function for checking the model is added

--- fasttext/fasttext_approach.py
+++ fasttext/fasttext_approach.py
@@ -16,8 +16,19 @@ print("Elapsed time for learning: {:.3f} sec".format(time.time() - start_time))
 
 def checking_the_model():
     model = fasttext.load_model('model.bin')
-    print u'что' in model
-    print(model[u'что'])
 
-    print u'сепулька' in model
-    print(model[u'сепулька'])
+    # toy model: '/home/ubuntu/PycharmProjects/DSM_morphology/fasttext/first_model_cbow/model.bin')
+    #print u'что' in model
+    #print(model[u'что'])
+
+    #print u'сепулька' in model
+    #print(model[u'сепулька'])
+
+    classic_model = gensim.models.Word2Vec.load('/home/ubuntu/model_w2v/word2vec_standart')
+
+    word_to_analyze = u'гнать' # input('type in a word to check: ')
+
+    print('similar words by vector are:',
+          classic_model.most_similar(positive=[numpy.array(model[word_to_analyze], dtype='float32')], topn = 5))
+
+checking_the_model()
