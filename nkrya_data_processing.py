150dd884d9578948237bd1ad158e93b601090ef3
Mikhail <mikeabyrvalg5@gmail.com>
2017-06-06 22:30:12 +0300

    Update nkrya_data_processing.py
    a script was changed in order to store all the output data in one text file

--- nkrya_data_processing.py
+++ nkrya_data_processing.py
@@ -4,7 +4,8 @@ import time
 #making data for word2vec
 start_time = time.time()
 lemmas = []
-index = 0
+
+"""index = 0
 with open('C:/Users/Ольга/Downloads/ruscorpora_lemmatized.json', encoding='utf-8') as json_opener:
     for linea in json_opener:
         line = json.loads(linea)
@@ -25,4 +26,28 @@ with open('C:/Users/Ольга/Downloads/ruscorpora_lemmatized.json', encoding='
                 writer.write('\n'.join([' '.join(sent).lower() for sent in lemmas]))
             print("Elapsed time for 100 thousand sents: {:.3f} sec".format(time.time() - start_time))
             lemmas = []
-            index += 1
+            index += 1"""
+
+with open('C:/Users/Ольга/Downloads/data.txt', 'w', encoding='utf-8') as writer: # output file
+
+    with open('C:/Users/Ольга/Downloads/ruscorpora_lemmatized.json', encoding='utf-8') as json_opener: #input json file
+
+        for linea in json_opener:
+            line = json.loads(linea)
+            lemma = []
+
+            for dict in line:
+                if dict['analysis']:
+                    lemma.append(dict['analysis'][0]['lex'])
+                else:
+                    lemma.append(dict['text'])
+
+            if len(lemma) < 3:  # skip all the sentences that have less than 3 words
+                pass
+            else:
+                lemmas.append(lemma)
+
+            if len(lemmas) == 100000:
+                writer.write('\n'.join([' '.join(sent).lower() for sent in lemmas]))
+                print("Elapsed time for 100 thousand sents: {:.3f} sec".format(time.time() - start_time))
+                lemmas = []
