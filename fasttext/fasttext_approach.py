c8ba2317b9c1efd1b0e9e3bdba1a4cb020856efd
sadov-m <juneau52@mail.ru>
2017-06-10 18:17:41 +0300

    fasttext model estimations added

--- fasttext/fasttext_approach.py
+++ fasttext/fasttext_approach.py
@@ -11,31 +11,39 @@ def training_the_model():
     # first training: '/home/ubuntu/PycharmProjects//DSM_morphology/data.txt'
     fasttext.skipgram(path, 'fasttext_model', dim=300, min_count=5, neg=15, minn=4, maxn=5)
 
-training_the_model()
+
+#training_the_model()
 print("Elapsed time for learning: {:.3f} sec".format(time.time() - start_time))
 
 
-# word_pairs should be loaded as a list in which every pair is a string
-# string should cointain two words separated by space or escape sequence (\t,\n)
 def checking_the_model(word_pairs_to_check, manual_input=False):
-    model = fasttext.load_model('model.bin')
+    model = fasttext.load_model('/media/ubuntu/14_0/fasttext_model.bin')#'model.bin')
 
     if manual_input:
+        print u'sorry, this function is currently unfinished'
         word_to_analyze = raw_input('type in a word to check: ')
         word_to_compare = raw_input('type in the second word: ')
 
     else:
 
-        for pair in word_pairs_to_check:
+        with open('fasttext_w2v_estimations.txt', 'w') as writer:
 
-            word_1, word_2 = pair.split()
+            for pair in word_pairs_to_check:
 
-            dataSetI = model[word_1]
-            dataSetII = model[word_2]
-            result = 1 - spatial.distance.cosine(dataSetI, dataSetII)
+                word_to_analyze, word_to_compare, value = pair.split('\t')
 
-            print(word_1, word_2,result)
-            print("Elapsed time for this word: {:.3f} sec".format(time.time() - start_time))
+                dataSetI = model[word_to_analyze]
+                dataSetII = model[word_to_compare]
+                result = 1 - spatial.distance.cosine(dataSetI, dataSetII)
+                writer.write(word_to_analyze + '\t' + word_to_compare + '\t' + str(result) + '\n')
+                #print(word_to_analyze, word_to_compare,result)
+            print("Elapsed time for estimation: {:.3f} sec".format(time.time() - start_time))
 
 #strings = [u'кумовство потакание', u'ребенок\tбаловаться', u'жизнь\nсуществование']
 #checking_the_model(strings)
+
+with open('/home/ubuntu/PycharmProjects/DSM_morphology/morfessor/'
+          'golden_standard_final.txt') as opener:
+    words_pairs = opener.read().split('\n')
+
+checking_the_model(words_pairs)
