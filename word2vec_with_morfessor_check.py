2852fae7503716f0bf0aab085685d65cad9fecfd
Mikhail <mikeabyrvalg5@gmail.com>
2017-06-10 16:46:39 +0300

    final version
    + script to retrieve estimations from morpho w2v model

--- word2vec_with_morfessor_check.py
+++ word2vec_with_morfessor_check.py
@@ -2,58 +2,68 @@ import gensim
 import morfessor
 import pymystem3
 import numpy
-# from scipy import spatial
 import time
+from scipy import spatial
 
 io = morfessor.MorfessorIO()
 model = gensim.models.Word2Vec.load('word2vec_morpho')
-classic_model = gensim.models.Word2Vec.load('word2vec_standart')
 analyzer = pymystem3.Mystem()
 start_time = time.time()
-word_for_analysis = input('type in a word: ')
 model_types = io.read_binary_model_file('morfessor/types')
 
-word_for_test = analyzer.lemmatize(word_for_analysis)[0]
-compounds = []
+# for manual input
+#word_for_analysis = input('type in a word: ')
+#word_to_compare = input('type in another word: ')
 
-try:
-    compounds.extend(model_types.segment(word_for_test))
-except KeyError:
-    compounds.extend(model_types.viterbi_segment(word_for_test)[0])
 
-vecs = []
-print('the compounds are:', compounds)
-
-# getting ready vecs for our target word
-for compound in compounds:
+def vec_for_word(word, return_sum=True):
+    word_for_test = word #analyzer.lemmatize(word)[0]
+    compounds = []
 
     try:
-        vecs.append(model.wv[compound])
-        values_type = set()
-        for digit in model.wv[compound]:
-            values_type.add(type(digit))
-        print(values_type)
-        # print('similar by compound:', model.wv.most_similar(positive=[compound]))
+        compounds.extend(model_types.segment(word_for_test))
     except KeyError:
-        vecs.append(numpy.array([0.0 for i in range(300)], dtype='float32'))
+        compounds.extend(model_types.viterbi_segment(word_for_test)[0])
 
-total_sum = numpy.array([0.0 for j in range(300)], dtype='float32')
-values_type = set()
-for digit in total_sum:
-    values_type.add(type(digit))
-print(values_type)
+    vecs = []
+    #print('the compounds are:', compounds)
 
-for ind, vec in enumerate(vecs):
-    total_sum += vec
-    # numpy.add(total_sum, vec)
+    # getting ready vecs for our target word
+    for compound in compounds:
 
-values_type = set()
-for digit in total_sum:
-    values_type.add(type(digit))
-print(values_type)
-total_average = total_sum/float(len(compounds))
+        try:
+            vecs.append(model[compound])
 
-print('similar by sum_vector are:', classic_model.wv.similar_by_vector(total_sum))
-print('similar by average_vector are:', classic_model.wv.similar_by_vector(total_average))
-print('similar by classic model are:', classic_model.wv.most_similar(positive=[word_for_test]))
-print("Elapsed time for this word: {:.3f} sec".format(time.time() - start_time))
+        except KeyError:
+            vecs.append(numpy.zeros(300, dtype='float32'))
+
+    total_sum = numpy.zeros(300, dtype='float32')
+
+    for ind, vec in enumerate(vecs):
+        total_sum += vec
+        # numpy.add(total_sum, vec)
+
+    total_average = total_sum/float(len(compounds))
+
+    if return_sum:
+        return total_sum
+    else:
+        return total_average
+
+
+def estimations_retrieval():
+
+    with open('morfessor/golden_standard_final.txt', encoding='utf-8') as opener:
+        words_pairs = opener.read().split('\n')
+
+    with open('morfessor/morpho_w2v_estimations.txt', 'w', encoding='utf-8') as writer:
+
+        for pair in words_pairs:
+
+            word_1, word_2, value = pair.split('\t')
+            dataSetI = vec_for_word(word_1)
+            dataSetII = vec_for_word(word_2)
+            result = 1 - spatial.distance.cosine(dataSetI, dataSetII)
+            writer.write(word_1+'\t'+word_2+'\t'+str(result)+'\n')
+
+    print("Elapsed time for estimation: {:.3f} sec".format(time.time() - start_time))
